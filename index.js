97980ee6a8cab5ee4e9727cb7ead2cf5db03d600
Kelli Kirk <kelli.kirk@voco.ee>
2024-10-04 18:24:42 +0300

    Parandatud todo_express

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
 
+const app = express();
 
-const app = express()
-const path = require('path')
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.set('view engine' , 'ejs')
-app.set('views', path.join(__dirname), 'views')
+app.get('/', (req, res) => {
+    res.render('index'); 
+});
 
-app.get('/' , (req, res) => {
-    res.send('test')
-})
-
-app.listen(3001, ()  => {
-    console.log('Server is started http://localhost:3001')
-} )
+app.listen(3001, () => {
+    console.log('Server is started http://localhost:3001');
+});
