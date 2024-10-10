dc767d323a4bb7febd07d3ae934b0e80e626dd97
Kelli Kirk <kelli.kirk@voco.ee>
2024-10-10 14:01:14 +0300

    Read data from file

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const fs = require('node:fs');
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    res.render('index'); 
+fs.readFile('./tasks', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const tasks = data.split('\n')  
+  res.render('index' ,{tasks: tasks} ); 
+});
+    
 });
 
 app.listen(3001, () => {
