7c39b919c070c6f16692d9f24ad828d5db999fbe
Kelli Kirk <kelli.kirk@voco.ee>
2024-10-11 17:42:39 +0300

    Write data to file

--- index.js
+++ index.js
@@ -12,46 +12,45 @@ const readFile = (filename) => {
         fs.readFile(filename, 'utf8', (err, data) => {
             if (err) {
                 console.error(err);
-                return res.status(500).send('Error reading tasks');
+                return reject('Error reading tasks');
             }
             const tasks = data.split('\n'); 
-            resolve(tasks)
+            resolve(tasks);
         });   
-    } )
-} 
+    });
+}; 
 
 app.use(express.urlencoded({ extended: true }));
 
+let tasks = []; 
+
 app.get('/', (req, res) => {
     readFile('./tasks')
-        .then(tasks => {
-            console.log(tasks)
-             res.render('index', { tasks: tasks });
-        }) 
+        .then(fileTasks => {
+            tasks = fileTasks; 
+            res.render('index', { tasks: tasks });
         })
-        
-    
+        .catch(err => {
+            res.status(500).send(err);
+        });
+});
 
 app.post('/', (req, res) => {
     console.log('form sent data');
     
-    tasks.push(req.body.task)
+    tasks.push(req.body.task);
     console.log(req.body.task);
-     const data = tasks.join('\n')
-     console.log(data)
-    console.log(tasks.join('\n'))
-
-     const tasks = data.split('\n');  
-  
     
-        fs.writeFile('./tasks', data, err => {
-            if (err) {
-              console.error(err);
-            } else {
-              res.redirect('/')
-            }
-          });
-        
+    const data = tasks.join('\n');
+    
+    fs.writeFile('./tasks', data, err => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error writing tasks');
+        } else {
+            res.redirect('/');
+        }
+    });
 });
 
 app.listen(3001, () => {
