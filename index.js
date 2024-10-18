48bc52ac99bca2199cfdea86edb4e216625d0d14
Kelli Kirk <kelli.kirk@voco.ee>
2024-10-18 10:13:17 +0300

    Refactor data to JSON and change data management

--- index.js
+++ index.js
@@ -1,47 +1,42 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
+const express = require('express')
+const path = require('path')
+const fs = require('node:fs');
 
-const app = express();
+const app = express()
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }))
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
 const readFile = (filename) => {
     return new Promise((resolve, reject) => {
         fs.readFile(filename, 'utf8', (err, data) => {
             if (err) {
-                console.error(err);
-                return reject('Error reading tasks');
+              console.error(err);
+              return;
             }
-            const tasks = JSON.parse(data); 
-            resolve(tasks);
-        });   
-    });
-}; 
-
-app.use(express.urlencoded({ extended: true }));
-
-let tasks = []; 
+            const tasks = JSON.parse(data) 
+            resolve(tasks)
+        });
+    })
+} 
 
 app.get('/', (req, res) => {
     readFile('./tasks.json')
-        .then(fileTasks => {
-            tasks = fileTasks; 
-            res.render('index', { tasks: tasks });
-        })
-        .catch(err => {
-            res.status(500).send(err);
-        });
-});
+        .then((tasks) => { 
+            res.render('index', {tasks: tasks})
+    })   
+})
 
 app.post('/', (req, res) => {
-    console.log('form sent data');
+    console.log('form sent data')
+    let task = req.body.task
     readFile('./tasks.json')
-        .then(tasks => {
+        .then((tasks) => {
             let index
-            if(tasks.length == 0) {
-                index = 1
+            if(tasks.length === 0){
+                index = 0
             } else {
                 index = tasks[tasks.length - 1].id + 1 
             } 
@@ -50,24 +45,22 @@ app.post('/', (req, res) => {
                 id: index,
                 task: task
             } 
-        } )
-    tasks.push(newTask);
-    console.log(tasks);
-    const data = JSON.stringify(tasks, null, 2)
-    console.log(data)
-    
-    const data = tasks.join('\n');
-    
-    fs.writeFile('./tasks.json', data, err => {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Error writing tasks');
-        } else {
-            res.redirect('/');
-        }
-    });
-});
+
+            tasks.push(newTask)
+            console.log(tasks)
+            
+            const data = JSON.stringify(tasks, null, 2)
+
+            fs.writeFile('./tasks.json', data, err => {
+                if (err) {
+                  console.error(err);
+                } else {
+                    res.redirect('/')
+                }
+            });
+    })
+})
 
 app.listen(3001, () => {
-    console.log('Server is started http://localhost:3001');
-});
+    console.log('Server is started http://localhost:3001')
+})
